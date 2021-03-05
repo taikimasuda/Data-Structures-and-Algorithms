b26376216f4597df29fa5634903e13195cb5e5bd
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-03-05 20:00:50 +0900

    update

---
+++ Big-O/Exercise1.js
@@ -0,0 +1,14 @@
+function funChallange (input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) { // O(n)
+    anotherFunction(); // O(n)
+    let starnger = true; // O(n)
+    a++; // O(n)
+  }
+
+  return a; //O(1)
+}
+
+funChallange(); // Time Complexity: O(3 + 4(or 3)n)
