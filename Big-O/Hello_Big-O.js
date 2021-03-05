9689ca07ccee97681da95d5097b7b46e2cba2020
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-03-05 18:46:17 +0900

    first commit

---
+++ Big-O/Hello_Big-O.js
@@ -0,0 +1,31 @@
+//#1 -- For loop in Javascript
+const fish = ['dory', 'bruce', 'marlin', 'nemo'];
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(1000).fill('nemo');
+
+function findNemo(fishes) {
+  let t0 = performance.now();
+  for (let i = 0; i < fishes.length; i++) {
+    if (fishes[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+  let t1 = performance.now();
+  console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
+}
+
+function getFirstFish(fishes) {
+  console.log(fishes[0]);
+}
+
+function getFirstTwoFish(fishes) {
+  console.log(fishes[0]);
+  console.log(fishes[1]);
+}
+
+// O(n) --> Linear Time
+findNemo(everyone); // Time Complexity: O(n)
+findNemo(large);  // Time Complexity: O(n)
+getFirstFish(everyone); // Time Complexity: O(1)
+getFirstTwoBoxes(fish) // Time Complexity: O(2)
