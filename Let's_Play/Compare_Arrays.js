ada7726dc2cca9f27fa07aa379b63753a3e7248e
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-03-07 15:58:22 +0900

    update

---
+++ Let's_Play/Compare_Arrays.js
@@ -0,0 +1,59 @@
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'i'];
+// should return false.
+// --------------
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'x'];
+// should return true.
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'x'];
+
+function containsCommonItem(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if(arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+containsCommonItem(array1, array2);
+
+// Time Complexity: O(a * b) --> Too slow
+// Space Complexity: O(1)
+
+function containsCommonItem2(arr1, arr2) {
+  // loop through first array and create object where properties === items in the array
+  // loop through second array and check if item in second array exists on created object.
+
+  let map = {};
+  for (let i = 0; i < arr1.length; i++) {
+    if(!map[i]) {
+      const item = arr1[i];
+      map[item] = true;
+    }
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if(map[arr2[j]]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+containsCommonItem2(array1, array2);
+
+// Time Complexity: O(a + b)
+// Space Complexity: O(a)
+
+function containsCommonItem3(arr1, arr2) {
+  return array2.some(item => array2.includes(item))
+}
+
+containsCommonItem3(array1, array2);
