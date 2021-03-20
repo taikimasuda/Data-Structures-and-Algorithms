68c24fbc6a6defdacd0ef5762a71f1332340a0d7
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-03-20 10:27:42 +0900

    added Arrays folder and introduction.js

---
+++ Arrays/Introduction.js
@@ -0,0 +1,16 @@
+const strings = ['a', 'b', 'c', 'd'];
+// 4*4 = 16 bytes of storage
+
+// push
+strings.push('e'); // O(1)
+
+// pop
+strings.pop(); // O(1)
+
+// unshift
+strings.unshift('x'); // O(n)
+
+// splice
+strings.splice(2, 0, 'y'); // O(n)
+
+console.log(strings);
